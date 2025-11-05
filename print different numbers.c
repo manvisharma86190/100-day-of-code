fbec476157058c9b17a28c34c566ba341006ef63
manvisharma86190 <manvisharma86190@gmail.com>
2025-11-05 23:59:16 +0530

    Add files via upload

---
+++ print different numbers.c
@@ -0,0 +1,23 @@
+/* Write a program to print the following pattern:
+5
+45
+345
+2345
+12345 */ 
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    // outer loop for rows
+    for(i = 5; i >= 1; i--) {
+        // inner loop to print numbers from i to 5
+        for(j = i; j <= 5; j++) {
+            printf("%d", j);
+        }
+        printf("\n"); // move to next line after each row
+    }
+
+    return 0;
+}
